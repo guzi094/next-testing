13d4fba087a9624192cba3ccbddaff14ad4e3a71
guzi094 <guzi2341@gmail.com>
2023-08-31 13:12:43 +0900

    add testing tool

---
+++ __tests__/Home.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react";
+import Home from "@/app/page";
+import "@testing-library/jest-dom";
+
+describe("Home", () => {
+  it("should have Docs text", () => {
+    render(<Home />);
+    const linkElement = screen.getByText("Docs");
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it("should contain the text 'information'", () => {
+    render(<Home />);
+    const linkElement = screen.getByText(/information/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+});
